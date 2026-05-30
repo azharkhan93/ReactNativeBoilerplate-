16a8c2cc684b3b56db16e6f7e48cb03c56360d25
azharkhan93 <azharkhan.work101@gmail.com>
2026-05-30 17:29:35 +0530

    disable inline style linting in VendorAnalyticsScreen

--- components/Vendor/VendorAnalyticsScreen/VendorAnalyticsScreen.tsx
+++ components/Vendor/VendorAnalyticsScreen/VendorAnalyticsScreen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
 import { View, ScrollView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
