c0662532c78d703bcb6d690667e19203c0e1ef91
azharkhan93 <azharkhan.work101@gmail.com>
2026-05-31 22:51:08 +0530

    update GRAPHQL_API_URL to use production endpoint in non-development builds

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,6 @@
 import { Platform } from 'react-native';
 
-export const GRAPHQL_API_URL =
-  Platform.OS === 'android'
-    ? 'http://10.0.2.2:4000/graphql'
-    : 'http://localhost:4000/graphql';
+export const GRAPHQL_API_URL = typeof __DEV__ !== 'undefined' && !__DEV__
+  ? 'https://nestjsgqltest.onrender.com/graphql'
+  : `http://${Platform.OS === 'android' ? '10.0.2.2' : 'localhost'}:4000/graphql`;
 
