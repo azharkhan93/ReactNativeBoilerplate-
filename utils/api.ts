e6eb890318e8f0716a517c75d7a89b427a5a33df
azharkhan93 <azharkhan.work101@gmail.com>
2026-07-19 15:25:46 +0530

    invert GraphQL API URL logic to improve readability and consistency

--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,7 @@
 import { Platform } from 'react-native';
 
-export const GRAPHQL_API_URL = typeof __DEV__ !== 'undefined' && !__DEV__
-  ? 'https://nestjsgqltest.onrender.com/graphql'
-  : `http://${Platform.OS === 'android' ? '10.0.2.2' : 'localhost'}:4000/graphql`;
-
+export const GRAPHQL_API_URL = __DEV__
+  ? `http://${
+      Platform.OS === 'android' ? '10.0.2.2' : 'localhost'
+    }:4000/graphql`
+  : 'https://nestjsgqltest.onrender.com/graphql';
