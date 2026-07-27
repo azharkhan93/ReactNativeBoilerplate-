7056f6973f00ac76611856211def98897a61d7e0
azharkhan93 <azharkhan.work101@gmail.com>
2026-07-27 23:27:05 +0530

    disable splash screen for testing and increment Android version code to 34

--- App.tsx
+++ App.tsx
@@ -5,18 +5,18 @@ import { ApolloProvider } from '@apollo/client/react';
 import { apolloClient } from '@/utils/apolloClient';
 import { AppNavigator } from '@/navigation/AppNavigator';
 import { KeyboardDismissView } from '@/components/theme';
-import { AnimatedSplashScreen, AppSkeletonLoader } from '@/components/shared';
+import { AppSkeletonLoader } from '@/components/shared';
 import { NotificationBanner } from '@/components/NotificationBanner';
 import { useAppInit } from '@/hooks/useAppInit';
 
 export default function App() {
-  const { showSplash, isAppReady, handleSplashFinish } = useAppInit();
+  const { isAppReady } = useAppInit();
 
   const renderContent = () => {
-   
-    if (showSplash) {
-      return <AnimatedSplashScreen onFinish={handleSplashFinish} />;
-    }
+    // Splash screen disabled for testing per user request
+    // if (showSplash) {
+    //   return <AnimatedSplashScreen onFinish={handleSplashFinish} />;
+    // }
 
     if (!isAppReady) {
       return <AppSkeletonLoader />;
